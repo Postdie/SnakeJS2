87c0c01f3849043635eaa9b3d6f70b0cb858230f
Postdie <fernandezdiego845@gmail.com>
2024-01-21 20:09:54 -0600

    Segundo Commit

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const lienzo = document.querySelector('#lienzo');
 const ctx = lienzo.getContext('2d');
 
+let posX = 0;
+let posY = 1;
+let direction = 1;
+
+const bgSound = new Audio('./8-bitgame-158815.mp3');
+const eating = new Audio('./coin-collect-retro-8-bit-sound-effect-145251.mp3');
+
+function init(){
+
+    posX = 0;
+    posY = 1;
+    direction = 1;
+
 const snake = []
 ctx.fillStyle = "black";
 ctx.fillRect(50, 30, 20, 20);
@@ -40,6 +53,11 @@ snake.push({
     }
 });
 
+return snake;
+}
+
+let snake = init();
+
 function nextMove(){
     snake.forEach((item, index) =>{
         if(index === 0){
@@ -61,26 +79,31 @@ const food = {
         ctx.fillText('🎵', this.x * 20, this.y * 20);
     },
     random: function(){
-        this.x = Math.floor(Math.random() * 30);
-        this.y = Math.ceil(Math.random() * 20);
+        this.x = Math.floor(Math.random() * 25);
+        this.y = Math.ceil(Math.random() * 17);
     }
 }
 
 function checkEat(){
     if(snake[0].x === food.x && snake[0].y === food.y){
+        eating.play();
         snake.push({ ...snake[1]});
         food.random();
     }
 }
 
-let posX = 0;
-let posY = 1;
-let direction = 1;
+function gameOver(){
+    for(let i =1; i < snake.length; i++){
+        if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+            return true;
+        }
+    }
+    return false;
+}
 
 food.random();
 setInterval(() => {
-    ctx.fillRect(0, 0, 600, 400);
-
+    ctx.fillRect(0, 0, 500, 340);
 
     food.pinta();
     snake.forEach(item => item.pinta());
@@ -92,29 +115,32 @@ setInterval(() => {
     else if(direction === 3) posX--;
     else posY--;
 
-    if(posX > 29) posX = 0;
-    else if(posX <= 0) posX = 30;
-    if(posY > 20) posY = 1;
-    else if(posY < 1) posY = 20;
+    if(posX > 24) posX = 0;
+    else if(posX <= 0) posX = 24;
+    if(posY > 17) posY = 1;
+    else if(posY < 1) posY = 17;
 
     nextMove();
 
 }, 200);
 
-document.querySelector('body')
-    .addEventListener('keydown', (e) => {
-        switch(e.key){
-            case 'ArrowRight':
+document.querySelector('container')
+    .addEventListener('click', (e) => {
+        if(e.target.classList.contains('btn')){
+            const button = e.target.innerText;
+        switch(button){
+            case 'Right':
                 direction = 1;
                 break;
-            case 'ArrowLeft':
+            case 'Left':
                 direction = 3;
                 break;
-            case 'ArrowDown':
+            case 'Down':
                 direction = 2;
                 break;
-            case 'ArrowUp':
+            case 'Up':
                 direction = 4;
                 break;
+        }
         }
     })
